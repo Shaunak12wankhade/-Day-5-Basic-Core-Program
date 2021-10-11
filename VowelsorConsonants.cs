e103705b89e6c19756843ca7657ed79e44573f97
unknown <shaunak12wankhade@ggmail.com>
2021-10-11 12:47:47 +0530

    [Shaunak] Added : Alphabet is Vowel or Consonant

---
+++ VowelsorConsonants.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic_core_program
+{
+    class VowelsorConsonants
+    {
+        static void Main(string[] args)
+        {
+            char a;
+            Console.WriteLine("Please enter the alphabet");
+            a = char.Parse(Console.ReadLine());
+
+            if (a == 'a'| a == 'e'| a == 'i'| a== 'o'| a == 'u')
+                {
+                   Console.WriteLine("Alphabet is a Vowel");
+                }
+            else
+            {
+                Console.WriteLine("Alphabet is a consonant");
+            }
+
+
+        }
+    }
+}
