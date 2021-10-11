313fb99a72fd591549e0f37ec671df380c93a2f4
unknown <shaunak12wankhade@ggmail.com>
2021-10-11 11:57:49 +0530

    [Shaunak] Added : Number is Even or odd problem

---
+++ OddorEven.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic_core_program
+{
+    class OddorEven
+    {
+        static void Main(string[] args)
+        {
+            int num = 0;
+            int result = 0;
+            Console.WriteLine("Enter no.");
+            num = int.Parse(Console.ReadLine());
+            {
+                result = num % 2;
+            }
+
+            if (result == 0)
+            {
+                Console.WriteLine("No is even");
+            }
+            else
+            {
+                Console.WriteLine("No is odd");
+            }
+        }
+    }
+}
