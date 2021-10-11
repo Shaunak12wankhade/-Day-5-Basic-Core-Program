2d26375240300704c116e3fde755cbde3c6d2619
unknown <shaunak12wankhade@ggmail.com>
2021-10-11 11:39:37 +0530

    [Shaunak] Added : Quotient and remainder problem

---
+++ Quotient and remainder.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic_core_program
+{
+    class Quotient_and_remainder
+    {
+        static void Main(string[] args)
+        {
+
+            int Num = 1;
+            int i = 1;
+            int Div = 1;
+            Console.WriteLine("Enter the Number");
+            Num = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter the divider ");
+            Div = int.Parse(Console.ReadLine());
+
+            double Quotient = (Num / Div);
+            Console.WriteLine("Your Quotient is = " + Quotient);
+
+            double Remainder = (Num % Div);
+            Console.WriteLine("Your Remainder is = " + Remainder);
+
+        }
+    }
+}
